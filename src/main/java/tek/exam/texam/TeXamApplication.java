10300968d8815c0e101bb66439c0cfbfa9f2113c
Mads Møller <67467953+MadsMoeller@users.noreply.github.com>
2022-01-25 08:54:44 +0100

    Initial commit

---
+++ src/main/java/tek/exam/texam/TeXamApplication.java
@@ -0,0 +1,13 @@
+package tek.exam.texam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeXamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeXamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tek/exam/texam/TeXamApplicationTests.java
@@ -0,0 +1,13 @@
+package tek.exam.texam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeXamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
